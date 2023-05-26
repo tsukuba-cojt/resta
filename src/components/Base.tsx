fbff9fb6f3fefe87e49009ed4b0df7aa0fff57c0
itsu-dev <nomaki@spookies.co.jp>
2023-05-26 17:18:34 +0900

    update

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+  position: fixed;
+  width: 300px;
+  height: 100%;
+  bottom: 0;
+  right: 0;
+  background: white;
+  z-index: 999;
 `;
 
 const Base = () => {
     return (
         <Wrapper>
-
+        laugcouagcouafgou
         </Wrapper>
     )
 };

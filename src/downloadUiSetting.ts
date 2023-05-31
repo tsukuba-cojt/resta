209bfd69659b5dbfdfdfa297e2842e3bf38fcd11
{Shore} <{kisidakazuyuki@gmail.com}>
2023-05-31 14:29:58 +0900

    Rewrite t() and create downloadLangJson()

--- src/downloadUiSetting.ts
+++ src/downloadUiSetting.ts
@@ -1,8 +1,16 @@
-import {RestaStyle, RestaStyles} from "./types/RestaSetting";
+import { Language } from './types/Language';
+import { RestaStyle, RestaStyles } from './types/RestaSetting';
 
 export const downloadUiSetting = async (): Promise<RestaStyle[]> => {
-    const endpoint =
-        'https://gist.githubusercontent.com/itsu-dev/80b3b29608fb11a697a1c98fb84cba91/raw/b658d3f27b658a28633eb75fe4cc0700c7e80732/resta-setting.json';
-    const json = await (await fetch(endpoint)).json();
-    return (json as RestaStyles).styles;
+  const endpoint =
+    'https://raw.githubusercontent.com/K-Kazuyuki/resta-ui-setting/main/resta-setting.json';
+  const json = await (await fetch(endpoint)).json();
+  return (json as RestaStyles).styles;
+};
+
+export const downloadLangJson = async () => {
+  const endpoint =
+    'https://raw.githubusercontent.com/K-Kazuyuki/resta-ui-setting/main/lang.json';
+  const json = await (await fetch(endpoint)).json();
+  return json as Language;
 };

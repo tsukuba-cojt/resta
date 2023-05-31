dbbcf0919e850e30a46d866906d3e7f9fb50d50e
itsu-dev <nomaki@spookies.co.jp>
2023-05-31 15:28:31 +0900

    言語キーが見つからない時にはキーを返すように変更

--- src/utils/translator.ts
+++ src/utils/translator.ts
@@ -1,18 +1,27 @@
 // @ts-ignore
 import lang from '../consts/lang.json';
-import { i18n } from 'webextension-polyfill';
-import { Language } from '../types/Language';
+import {i18n} from 'webextension-polyfill';
+import {Language} from '../types/Language';
 
 // 引数持ち
 const t = (lang: Language, key: string, ...params: any) => {
-  const s = lang[i18n.getUILanguage()][key];
-  if (params.length == 0 || s) {
-    return s ? s : key;
-  }
+    if (!lang[i18n.getUILanguage()]) {
+        return `${i18n.getUILanguage()}.${key}`;
+    }
 
-  // 置き換え処理
-  const format = require('string-format');
-  return format(s, ...params);
+    const s = lang[i18n.getUILanguage()][key];
+
+    if (!s) {
+        return `${i18n.getUILanguage()}.${key}`;
+    }
+
+    if (params.length == 0 || s) {
+        return s ? s : key;
+    }
+
+    // 置き換え処理
+    const format = require('string-format');
+    return format(s, ...params);
 };
 
 export default t;

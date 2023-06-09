e904a53d29c0c5c1c70f2000e00a6e70647ccaea
{Shore} <{kisidakazuyuki@gmail.com}>
2023-06-09 14:38:32 +0900

    fix

--- src/utils/CSSUtils.ts
+++ src/utils/CSSUtils.ts
@@ -17,48 +17,47 @@ export const kebabToCamel = (text: string): string => {
  * @param element
  */
 export const getAbsoluteCSSSelector = (element: HTMLElement): string => {
-  let result = "";
+  let result = '';
   const visit = (e: HTMLElement | null) => {
     if (e == null) return;
     result = `${createSelector(e)} > ${result}`;
     visit(e.parentElement);
-  }
+  };
 
   result = element.tagName.toLowerCase();
   visit(element.parentElement);
 
   return result;
-}
+};
 
 const createSelector = (element: HTMLElement): string => {
   const tag = element.tagName.toLowerCase();
 
-  if (element.getAttribute("id")) {
-    return `#${element.getAttribute("id")}`;
+  if (element.getAttribute('id')) {
+    return `#${element.getAttribute('id')}`;
   }
 
   const returnWithAttr = (attrName: string): string => {
     return `${tag}[${attrName}*="${element.getAttribute(attrName)!.trim()}"]`;
-  }
+  };
 
-  if (element.getAttribute("href")) {
-    return returnWithAttr("href");
-
-  } else if (element.getAttribute("src")) {
-    return returnWithAttr("src");
-
-  } else if (element.getAttribute("class")) {
-    return returnWithAttr("class");
+  if (element.getAttribute('href')) {
+    return returnWithAttr('href');
+  } else if (element.getAttribute('src')) {
+    return returnWithAttr('src');
+  } else if (element.getAttribute('class')) {
+    return returnWithAttr('class');
   }
 
   if (element.parentElement) {
     let i = 1;
     for (const node of Array.from(element.parentElement.childNodes)) {
       if (node == element) {
-        return `${tag}:nth-of-type(${i})`
+        return `${tag}:nth-of-type(${i})`;
       }
+      i++;
     }
   }
 
   return tag;
-}
+};

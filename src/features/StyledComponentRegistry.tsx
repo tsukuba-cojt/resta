fb56db6fb9f3b543c531cd0c197c8a4e92962b51
itsu-dev <itsu020402@gmail.com>
2023-07-21 22:39:05 +0900

    共有サイトとスタイルを統合

--- src/features/StyledComponentRegistry.tsx
+++ src/features/StyledComponentRegistry.tsx
@@ -1,14 +1,17 @@
 import {ConfigProvider} from "antd";
 import React from "react";
+import {COLOR_PRIMARY} from "../consts/styles";
+import GlobalStyle from "./GlobalStyle";
 
 export default function ({ children }: { children: React.ReactNode }) {
     return (
         <ConfigProvider
             theme={{
                 token: {
-                    colorPrimary: '#00b7ee',
+                    colorPrimary: COLOR_PRIMARY,
                 }
             }}>
+            <GlobalStyle />
             {children}
         </ConfigProvider>
     )

96303209646a731c571f57ab68ebc42bb675d850
itsu-dev <itsu020402@gmail.com>
2023-07-22 00:21:20 +0900

    hotfix: スタイル選択ダイアログが開かない問題を修正

--- src/features/upload_manager.ts
+++ src/features/upload_manager.ts
@@ -1,11 +1,22 @@
 import {getFormatByURL} from "./output_style";
 
 export const enableRestaAddStyleButton = (onClick: () => void) => {
-    const addButton = document.getElementById('resta-add-style');
-    if (addButton) {
-        addButton.style.display = "block";
-        addButton.addEventListener('click', onClick);
-    }
+    const mutationObserver = new MutationObserver(() => {
+        const addButton = document.getElementById('resta-add-style');
+        if (addButton) {
+            addButton.style.display = "block";
+            addButton.addEventListener('click', onClick);
+            mutationObserver.disconnect();
+        }
+    });
+
+    mutationObserver.observe(
+        document.getElementById('__next')!,
+        {
+            childList: true,
+            subtree: true
+        }
+    );
 }
 
 export const injectStyleJson = async (url: string) => {

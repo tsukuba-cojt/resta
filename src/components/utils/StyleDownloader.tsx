2bce7c5a8d2203ce5a78bff09baf110ac2e881d2
itsu-dev <itsu020402@gmail.com>
2024-01-19 17:42:41 +0900

    hotfix: スタイルがダウンロードできない問題の修正

--- src/components/utils/StyleDownloader.tsx
+++ src/components/utils/StyleDownloader.tsx
@@ -1,13 +1,53 @@
 import { notification } from 'antd';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import {
-  downloadFormat,
+  DOWNLOAD_PAGE_URL,
   enableRestaDownloadStyleButton,
+  ID_FORMAT_AUTHOR_INPUT,
+  ID_FORMAT_ID_INPUT,
+  ID_FORMAT_IMAGE,
+  ID_FORMAT_JSON_INPUT,
+  ID_FORMAT_TITLE
 } from '../../features/upload_import_manager';
 import t from '../../features/translator';
+import usePropsContext, { PropsContext } from '../../contexts/PropsContext';
+import { ImportedFormatAbstract, importFormat } from '../../features/importStyle';
+import { error } from '../../features/resta_console';
 
 const StyleDownloader = () => {
   const [api, contextHolder] = notification.useNotification();
+  const prop = usePropsContext();
+
+  const downloadFormat = async (): Promise<
+    ImportedFormatAbstract | undefined
+  > => {
+    const getValue = (id: string) => {
+      return (document.getElementById(id) as HTMLInputElement).value;
+    };
+
+    const title = document.getElementById(ID_FORMAT_TITLE)?.innerText;
+    const json = getValue(ID_FORMAT_JSON_INPUT);
+    const id = getValue(ID_FORMAT_ID_INPUT);
+    const imageUrl =
+      document.getElementById(ID_FORMAT_IMAGE)?.getAttribute('src') ?? undefined;
+    const downloadUrl = `${DOWNLOAD_PAGE_URL}/${id}`;
+    const author = document.getElementById(ID_FORMAT_AUTHOR_INPUT)?.innerText;
+
+    if (!title || !json || !id || !downloadUrl) {
+      error('error: ', title, json, id);
+      return undefined;
+    }
+
+    await importFormat(downloadUrl, title, json, id, imageUrl, author, prop);
+
+    return {
+      title,
+      id,
+      downloadUrl,
+      imageUrl,
+      author,
+    };
+  };
 
   useEffect(() => {
     enableRestaDownloadStyleButton(() => {
@@ -31,7 +71,11 @@ const StyleDownloader = () => {
     });
   }, []);
 
-  return contextHolder;
+  return (
+    <PropsContext.Provider value={prop}>
+      {contextHolder}
+    </PropsContext.Provider>
+  );
 };
 
 export default StyleDownloader;
--- src/features/upload_import_manager.tsx
+++ src/features/upload_import_manager.tsx
@@ -1,13 +1,10 @@
 import { getFormatByURL } from './output_style';
-import { ImportedFormatAbstract, importFormat } from './importStyle';
-import React, { useContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import StyledComponentRegistry from '../components/utils/StyledComponentRegistry';
 import StyleSelectionDialogRoot from '../components/utils/StyleSelectionDialogRoot';
 import StyleDownloader from '../components/utils/StyleDownloader';
 import { DEBUG_MODE } from '../consts/debug';
-import { error } from './resta_console';
-import { PropsContext } from '../contexts/PropsContext';
 
 const HOST = 'resta-frontend.pages.dev';
 export const DOWNLOAD_PAGE_URL = `https://${HOST}/style`;
@@ -40,38 +37,6 @@ const enableButton = (id: string, onClick: VoidFunction) => {
     childList: true,
     subtree: true,
   });
-};
-
-const getValue = (id: string) => {
-  return (document.getElementById(id) as HTMLInputElement).value;
-};
-
-export const downloadFormat = async (): Promise<
-  ImportedFormatAbstract | undefined
-> => {
-  const title = document.getElementById(ID_FORMAT_TITLE)?.innerText;
-  const json = getValue(ID_FORMAT_JSON_INPUT);
-  const id = getValue(ID_FORMAT_ID_INPUT);
-  const imageUrl =
-    document.getElementById(ID_FORMAT_IMAGE)?.getAttribute('src') ?? undefined;
-  const downloadUrl = `${DOWNLOAD_PAGE_URL}/${id}`;
-  const author = document.getElementById(ID_FORMAT_AUTHOR_INPUT)?.innerText;
-  const prop = useContext(PropsContext);
-
-  if (!title || !json || !id || !downloadUrl) {
-    error('error: ', title, json, id);
-    return undefined;
-  }
-
-  await importFormat(downloadUrl, title, json, id, imageUrl, author, prop);
-
-  return {
-    title,
-    id,
-    downloadUrl,
-    imageUrl,
-    author,
-  };
 };
 
 export const enableRestaAddStyleButton = (onClick: VoidFunction) => {
@@ -110,7 +75,7 @@ export const activateRestaSubsystems = () => {
       insertComponent(<StyleSelectionDialogRoot />);
 
       // Restaのダウンロードページなら
-    } else if (url.pathname.match(/^\/style\/.+\/$/)) {
+    } else if (url.pathname.match(/^\/formats\/.+\/$/)) {
       insertComponent(<StyleDownloader />);
     }
   }

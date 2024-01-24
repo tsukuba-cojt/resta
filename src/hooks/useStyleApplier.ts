b1b76a8e89573c1a6d7e0eb08ac7e133167ed537
{Shore} <{kisidakazuyuki@gmail.com}>
2024-01-24 13:48:34 +0900

    fix bugs

--- src/hooks/useStyleApplier.ts
+++ src/hooks/useStyleApplier.ts
@@ -3,7 +3,7 @@ import { PropsContext } from '../contexts/PropsContext';
 import { setFormatsAndPushToAry } from '../features/formatter';
 import { StyleRule } from '../features/style_sheet';
 import { ElementSelectionContext } from '../contexts/ElementSelectionContext';
-import { getAbsoluteCSSSelector } from '../utils/CSSUtils';
+import { getCssSelector } from '../utils/CSSUtils';
 
 type ReturnType = {
   /**
@@ -11,8 +11,11 @@ type ReturnType = {
    * @param keyOrArray CSSキーまたはCSSキーと値の配列
    * @param value （第一引数にCSSキーを指定した場合）CSSキーに対応する値
    */
-  applyStyle: (keyOrArray: string | [key: string, value: string | number][], value?: string | number) => void;
-}
+  applyStyle: (
+    keyOrArray: string | [key: string, value: string | number][],
+    value?: string | number,
+  ) => void;
+};
 
 export default function useStyleApplier(): ReturnType {
   const propsContext = useContext(PropsContext);
@@ -33,54 +36,79 @@ export default function useStyleApplier(): ReturnType {
     };
   };
 
-  const applyStyle = useCallback((keyOrArray: string | [key: string, value: string | number][], value: string | number | undefined) => {
-    debounce(() => {
-      if (elementContext.selectedElement == null) {
-        return;
-      }
+  const applyStyle = useCallback(
+    (
+      keyOrArray: string | [key: string, value: string | number][],
+      value: string | number | undefined,
+    ) => {
+      debounce(() => {
+        if (elementContext.selectedElement == null) {
+          return;
+        }
 
-      const keyString = Array.isArray(keyOrArray) ? keyOrArray.map(([key, _]) => `${key}`).join(';') : keyOrArray;
-      const id = previousCssKey.current === keyString ? previousId.current : Math.floor(Math.random() * 10000);
+        const keyString = Array.isArray(keyOrArray)
+          ? keyOrArray.map(([key, _]) => `${key}`).join(';')
+          : keyOrArray;
+        const id =
+          previousCssKey.current === keyString
+            ? previousId.current
+            : Math.floor(Math.random() * 10000);
 
-      let rules: StyleRule[];
-      if (Array.isArray(keyOrArray)) {
-        rules = keyOrArray.map(([key, value]) => {
-          return {
-            id,
-            cssSelector: getAbsoluteCSSSelector(elementContext.selectedElement!),
-            values: [{
-              key,
-              value: `${value}`
-            }]
-          };
+        let rules: StyleRule[];
+        if (Array.isArray(keyOrArray)) {
+          rules = keyOrArray.map(([key, value]) => {
+            return {
+              id,
+              cssSelector:
+                getCssSelector(
+                  elementContext.selectElementBy,
+                  elementContext.selectedElement,
+                ) + elementContext.selectedPseudoClass,
+              values: [
+                {
+                  key,
+                  value: `${value}`,
+                },
+              ],
+            };
+          });
+        } else {
+          rules = [
+            {
+              id,
+              cssSelector:
+                getCssSelector(
+                  elementContext.selectElementBy,
+                  elementContext.selectedElement,
+                ) + elementContext.selectedPseudoClass,
+              values: [
+                {
+                  key: keyOrArray,
+                  value: value != null ? `${value}` : 'unset',
+                },
+              ],
+            },
+          ];
+        }
+
+        setFormatsAndPushToAry(rules, propsContext);
+
+        previousCssKey.current = keyString;
+        previousId.current = id;
+
+        elementContext.overlayElements.forEach((overlay) => {
+          const rect = overlay.element.getBoundingClientRect();
+          overlay.overlayElement.style.top = `${rect.top}px`;
+          overlay.overlayElement.style.left = `${rect.left}px`;
+          overlay.overlayElement.style.width = `${rect.width}px`;
+          overlay.overlayElement.style.height = `${rect.height}px`;
         });
-      } else {
-        rules = [{
-          id,
-          cssSelector: getAbsoluteCSSSelector(elementContext.selectedElement),
-          values: [{
-            key: keyOrArray,
-            value: value != null ? `${value}` : 'unset',
-          }]
-        }];
-      }
-
-      setFormatsAndPushToAry(rules, propsContext);
-
-      previousCssKey.current = keyString;
-      previousId.current = id;
-
-      elementContext.overlayElements.forEach((overlay) => {
-        const rect = overlay.element.getBoundingClientRect();
-        overlay.overlayElement.style.top = `${rect.top}px`;
-        overlay.overlayElement.style.left = `${rect.left}px`;
-        overlay.overlayElement.style.width = `${rect.width}px`;
-        overlay.overlayElement.style.height = `${rect.height}px`;
-      });
-    }, 500)();
-  }, [elementContext.selectedElement, propsContext]);
+      }, 500)();
+    },
+    [elementContext.selectedElement, propsContext],
+  );
 
   return {
-    applyStyle
+    applyStyle,
   };
 }
